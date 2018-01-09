270bb7b1497bb6d87dc8b606c0c122744c6d137a
BrandonBartram98 <35037470+BrandonBartram98@users.noreply.github.com>
2018-01-09 20:16:48 +0000

    Website Final?

--- JS/storeJavaScript.js
+++ JS/storeJavaScript.js
@@ -1,7 +1,11 @@
 //Store Page JavaScript
 
-var data = {"total":0,"rows":[]};
+var data = {"cartTotal":0,"rows":[]};
 var totalCost = 0;
+
+function clearClick(){
+    localStorage.clear();
+}
 
 $(function()
 {
@@ -11,22 +15,22 @@ $(function()
 			});
 			
 			// check that the browser understands storage and check for a stored item by its name
-			if(localStorage &&  ?? ){
+			if(localStorage &&  localStorage.getItem('shopCart'))
+			{
+				console.log(localStorage.getItem('shopCart'));
+				data = JSON.parse(localStorage.getItem('shopCart'));
+				totalCost = parseFloat(localStorage.getItem('cartTotal'));
 				// console log localStorage.getItem('cart') value, you will see a long json string
 				// parse json from stting into an array object
-  			data=//change this 
   			
 			console.log(data);
-			//$('#cartcontent').datagrid('loadData', data);
-			
-			///update  .total html() or text()
-  			//localStorage.clear();
-
-				}
+			$('#cartProducts').datagrid('loadData', data);
+			$('div.shopCart .cartTotal').html('Total: £'+totalCost);
+			}
 			
 			
 			
-			// items to drag
+			// Dragable Products
 			$('.product').draggable({
 				revert:true,
 				proxy:'clone',
@@ -36,44 +40,44 @@ $(function()
 				
 			});
 			
-			//item to drop
+			// Products dropped to cart
 			$('.shopCart').droppable({
 				onDrop:function(e,source){
-					var name = $(source).find('p.productName').text();
-					var price = $(source).find('p.productPrice').attr("data");
-					addProduct(name, parseFloat(price));
+					var product = $(source).find('p.productTitle').text();
+					var price = $(source).find('p.productTitle').attr("data");
+					addProduct(product, parseFloat(price));
 				}
 			});
 		});
 		
-		function addProduct(name,price){
+		function addProduct(product,price){
 			function add(){
-				for(var i=0; i<data.total; i++){
+				for(var i=0; i<data.cartTotal; i++){
 					var row = data.rows[i];
-					if (row.name == name){
+					if (row.product == product){
 						row.quantity += 1;
 						return;
 					}
 				}
-				data.total += 1;
-				data.rows.push({
-					name:name,
+				data.cartTotal += 1;
+				data.rows.push
+				({
+					product:product,
 					quantity:1,
 					price:price
 				});
 			}
 			add();
 			totalCost += price;
-			//load data grid from jquery ui
 			$('#cartProducts').datagrid('loadData', data);
-			//update totals in the html
-			$('div.shopCart .total').html('Total: £'+totalCost);
+			$('div.shopCart .cartTotal').html('Total: £'+totalCost);
 			
-			// add to local storage
-			//stringify js array object into json and log it (var ??? = JSON.stringify(obj);)
-			console.log(data);
-	
-		
-		//set local storage value to the new json string
-
+			// Set local storage
+			if(localStorage)
+			{
+				localStorage.setItem('shopCart', JSON.stringify(data));
+				localStorage.setItem('cartTotal', totalCost);
+			}
+			console.log(JSON.stringify(data)); // JS array is stringified and logged
+			console.log(totalCost);
 		}
